0125f31e0efccb6e67c0b08685b8d6b783a77674
abraham <abraham.nacimba5566@utc.edu.ec>
2025-01-09 21:38:30 -0500

    ta

--- Camping/settings.py
+++ Camping/settings.py
@@ -32,7 +32,8 @@ DEBUG = True
 
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['camping-dfae.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'camping-dfae.onrender.com').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'camping-dfae.onrender.com']
 
 
 # Application definition
