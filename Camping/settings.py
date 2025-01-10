455b1ae7c4370864939ac3c705a0aade0cee4128
abraham <abraham.nacimba5566@utc.edu.ec>
2025-01-09 21:08:01 -0500

    cambio

--- Camping/settings.py
+++ Camping/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
